5791563591cdf8f35bdd661f5131a52727672740
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2026-03-21 19:08:09 +0100

    enable shared displaylink laptop support

--- modules/displaylink.nix
+++ modules/displaylink.nix
@@ -6,28 +6,31 @@
 
 let
   cfg = config.features.displaylink;
+  displaylinkUrl = "https://www.synaptics.com/sites/default/files/exe_files/2025-09/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.2-EXE.zip";
+  displaylinkHash = "sha256-JQO7eEz4pdoPkhcn9tIuy5R4KyfsCniuw6eXw/rLaYE=";
 in
 {
   options.features.displaylink = {
     enable = lib.mkEnableOption "DisplayLink dock support";
-    proprietaryUserspace.enable = lib.mkEnableOption ''
-      proprietary DisplayLink userspace support that requires the Synaptics
-      driver archive to be prefetched locally to satisfy the DisplayLink EULA
-    '';
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf cfg.enable {
-      boot = {
-        extraModulePackages = [ config.boot.kernelPackages.evdi ];
-        initrd.kernelModules = [ "evdi" ];
-      };
-    })
+  config = lib.mkIf cfg.enable {
+    nixpkgs.overlays = [
+      (final: prev: {
+        displaylink = prev.displaylink.overrideAttrs (_: {
+          src = final.fetchurl {
+            name = "displaylink-620.zip";
+            url = displaylinkUrl;
+            hash = displaylinkHash;
+          };
+        });
+      })
+    ];
 
-    (lib.mkIf cfg.proprietaryUserspace.enable {
-      # Keep the native GPU driver list intact and only prepend the DisplayLink driver.
-      services.xserver.videoDrivers = lib.mkBefore [ "displaylink" ];
-      systemd.services.dlm.wantedBy = [ "multi-user.target" ];
-    })
-  ];
+    services.xserver.videoDrivers = lib.mkBefore [ "displaylink" ];
+
+    # The upstream NixOS displaylink module defines the service but does not
+    # attach it to a target; enable it whenever DisplayLink support is requested.
+    systemd.services.dlm.wantedBy = [ "multi-user.target" ];
+  };
 }
